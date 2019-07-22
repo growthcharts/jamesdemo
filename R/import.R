50b7ed4a7f9ef062640358e2d1ae39de30806ab8
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-07-22 23:49:07 +0200

    Remove dependencies

--- R/import.R
+++ R/import.R
@@ -1,9 +1,2 @@
 #' @import     shiny
-#' @importFrom chartbox       load_chart
-#' @importFrom chartcatalog   create_chartcode parse_chartcode
-#' @importFrom chartplotter   placeholder set_curves
-#' @importFrom grDevices      palette
-#' @importFrom grid           grid.draw is.grob
-#' @importFrom methods        slot
-#' @importFrom minihealth     get_range convert_individual_bds
 NULL
