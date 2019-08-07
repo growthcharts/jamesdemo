c53e93755ac668119a719df785de25736db0e0aa
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-08-07 13:10:17 +0200

    Remove minihealth dependency

--- R/import.R
+++ R/import.R
@@ -1,6 +1,5 @@
 #' @import     shiny
 #' @importFrom jamesclient    request_site
-#' @importFrom minihealth     convert_individual_bds
 #' @importFrom shinydashboard dashboardBody dashboardHeader
 #'                            dashboardPage dashboardSidebar
 NULL
