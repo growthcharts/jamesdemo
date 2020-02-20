f389e18bfe50411470e9a7f9ef126daefd09c55f
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-02-20 15:37:13 +0100

    Give package its own cabinets

---
+++ R/installed.R
@@ -0,0 +1,8 @@
+#' Installed user cabinets
+#'
+#' A data set containing several user cabinets in which edits can be
+#' made.
+#'
+#' @format A list of \code{cabinet} objects. The \code{cabinet} object is
+#' defined in the \code{minihealth} package.
+"installed.cabinets"
