78408872c9b6e9657ce54e734b43a8a69069015b
Stef van Buuren <stef.vanbuuren@tno.nl>
2022-04-04 12:11:57 +0200

    Register deployment for JAMES 1.2.0

--- R/deploy.R
+++ R/deploy.R
@@ -2,6 +2,7 @@
 #'
 #' @export
 deploy <- function() {
+  cat("Before you do this, use devtools::install_github('growthcharts/pkg') with pkg all dependencies.\n" )
   deployApp(appDir = system.file("chart", package = "jamesdemo"),
             appTitle =  "JAMES tryout",
             account = "tnochildhealthstatistics")
